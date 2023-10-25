aec47c28da948d8b1d79fec8db05bb09983e1496
Alfredo GM <alfredogonzalezm265@gmail.com>
2023-10-25 14:33:20 -0600

    Creacion de qr

--- assets/js/qr.js
+++ assets/js/qr.js
@@ -1,12 +1,25 @@
 console.log("Entro qr.js");
 
 let imageqr = document.getElementById("imageqr");
+let txtContenido = document.getElementById("txtContenido");
+let descargarCodigo = document.getElementById("descargarCodigo");
 
-const size = "300x300";
-const qrtext = "Jesus";
+const size = "150x200";
+const qrtext = "Example";
 
-fetch(`https://api.qrserver.com/v1/create-qr-code/?size=${size}&data=${qrtext}`, {method: "GET"})
-    .then(data => {
-        console.log(data);
-        imageqr.src = data.url;
-    }).catch(error => console.log(error))
+
+function cargarQR(size, qrtext){
+    fetch(`https://api.qrserver.com/v1/create-qr-code/?size=${size}&data=${qrtext}`, {method: "GET"})
+        .then(data => {
+            console.log(data);
+            imageqr.src = data.url;
+            descargarCodigo.href = data.url;
+        }).catch(error => console.log(error))
+}
+
+    txtContenido.addEventListener("keypress", (event)=>{
+        if (event.key === "Enter"){
+            cargarQR("150x200",txtContenido.value);
+        }
+    })
+    cargarQR(size, qrtext);
